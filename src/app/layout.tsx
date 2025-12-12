5e81276a36921252b6df057359e4fbab32db6773
AYOUYA YA <rheyahyathebboss@gmail.com>
2025-12-12 15:19:36 +0100

    facebook pixel

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -24,15 +24,48 @@ export default function RootLayout({
   return (
     <html lang="en" className={`${geist.variable}`}>
       <body>
+        {/* Meta Pixel Code */}
+        {Boolean(process.env.NEXT_PUBLIC_ENABLE_ANALYTICS) && (
+          <>
+            <Script
+              id="meta-pixel"
+              strategy="afterInteractive"
+              dangerouslySetInnerHTML={{
+                __html: `
+              !function(f,b,e,v,n,t,s)
+              {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+              n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+              if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+              n.queue=[];t=b.createElement(e);t.async=!0;
+              t.src=v;s=b.getElementsByTagName(e)[0];
+              s.parentNode.insertBefore(t,s)}(window, document,'script',
+              'https://connect.facebook.net/en_US/fbevents.js');
+              fbq('init', '1189205430006739');
+              fbq('track', 'PageView');
+            `,
+              }}
+            />
+            <noscript>
+              <img
+                height="1"
+                width="1"
+                style={{ display: "none" }}
+                src="https://www.facebook.com/tr?id=1189205430006739&ev=PageView&noscript=1"
+                alt=""
+              />
+            </noscript>
+            <Script
+              src="https://analytics.rendy.io/api/script.js"
+              data-session-replay="true"
+              data-track-errors="true"
+              data-site-id="2"
+              defer
+            />
+          </>
+        )}
+        {/* End Meta Pixel Code */}
         <TRPCReactProvider>{children}</TRPCReactProvider>
         <Toaster />
-        <Script
-          src="https://analytics.rendy.io/api/script.js"
-          data-session-replay="true"
-          data-track-errors="true"
-          data-site-id="2"
-          defer
-        />
       </body>
     </html>
   );
